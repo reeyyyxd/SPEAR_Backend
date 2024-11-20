9b78977d84c55471f4c4d9bdccdb8b82c424153a
reeyyyxd <2woaranthony@gmail.com>
2024-11-21 06:12:37 +0800

    fix get-all-users

--- src/main/java/com/group2/SPEAR_Backend/Controller/ClassesController.java
+++ src/main/java/com/group2/SPEAR_Backend/Controller/ClassesController.java
@@ -10,43 +10,56 @@ import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:5173")
-@RequestMapping("/teacher")
 public class ClassesController {
+
+
+    //remove requestmapping
+    // all teacher's control = put /teacher
+    // all student's control = put /student
+    //TODO: enroll the fucking students
+
 
     @Autowired
     private ClassesService classesService;
 
 
-    @PostMapping("/createClass")
+    @PostMapping("/teacher/createClass")
     public ResponseEntity<ClassesDTO> createClass(@RequestBody ClassesDTO classRequest) {
         ClassesDTO response = classesService.createClass(classRequest);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
 
-    @GetMapping("/getallclasses")
+    @GetMapping("/teacher/getallclasses")
     public ResponseEntity<ClassesDTO> getAllClasses() {
         ClassesDTO response = classesService.getAllClasses();
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
 
-    @PutMapping("/updateClass/{id}")
+    @PutMapping("/teacher/updateClass/{id}")
     public ResponseEntity<ClassesDTO> updateClass(@PathVariable Long id, @RequestBody ClassesDTO updatedClass) {
         ClassesDTO response = classesService.updateClass(id, updatedClass);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
-    @DeleteMapping("/deleteClass/{id}")
+    @DeleteMapping("/teacher/deleteClass/{id}")
     public ResponseEntity<ClassesDTO> deleteClass(@PathVariable Long id) {
         ClassesDTO response = classesService.deleteClass(id);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
 
-    @GetMapping("/getclassKey/{courseCode}")
+    @GetMapping("/teacher/getclassKey/{courseCode}")
     public ResponseEntity<ClassesDTO> getClassKeyByCourseCode(@PathVariable String courseCode) {
         ClassesDTO response = classesService.getClassKeyByCourseCode(courseCode);
         return ResponseEntity.status(response.getStatusCode()).body(response);
     }
+
+
+
+
+
+
+
 
 }
